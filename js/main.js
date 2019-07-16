74eee5660f9b669e9c3e48c77cc44c2b2951c471
Denis <den.shempel@gmail.com>
2019-07-16 14:16:29 +0300

    client-appreciation has been added, logo-slider has been added

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.slider-logo__main').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
